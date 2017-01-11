1b9b9f034311fb9cb28f5cbe1af37ce651faa868
Salomon Beza <beza.salomon@gmail.com>
2017-01-11 14:19:48 +0200

    Initial commit

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item1 = {name: "HP", "color" => "black", "type" => "Laptop", price: 300000}
+item2 = {name: "Macbook", "color" => "Silver", type = "Laptop", price: 500000}
+item3 = {name: "Dell", "color" => "red", "type" => "Laptop", "Price"=> 400000}
