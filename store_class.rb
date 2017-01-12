9832d236a9dd75829909db97410e80eee2570ee9
Salomon Beza <beza.salomon@gmail.com>
2017-01-12 11:08:27 +0200

    Created a new class exercises and updated the store_item

---
+++ store_class.rb
@@ -0,0 +1,46 @@
+#item1 = {"name" => "HP", "color" => "black", "type" => "Laptop", "price" => 300000}
+#item2 = {"name" => "Macbook", "color" => "Silver", "type" => "Laptop", "price" => 500000}
+#item3 = {"name" => "Dell", "color" => "red", "type" => "Laptop", :Price => 400000}
+
+#puts item1[price
+#{:a => 123}
+
+class Store
+
+def initialize (description, color, type, price)
+	@description = description
+	@color = color
+	@type = type
+	@price = price
+end
+
+def description
+	@description
+end
+
+def price
+	@price
+end
+
+def price=(price)
+	@price=price
+end
+
+def type
+	@type
+end
+
+end
+
+product1 = Store.new("HP", "black", "Laptop", 300000)
+product1.price = 500000
+puts product1.description
+product2 = Store.new("Macbook", "Silver", "Laptop", 500000)
+#puts product1.price
+product3 = Store.new("Dell", "Red", "Laptop", 400000)
+
+puts "#{product1.description} computer with type: #{product1.type} costs: #{product1.price}"
+puts product2.price
+puts product3.price
+
+#product2 = Product.new ("Mackbook", )
--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,7 @@
-item1 = {name:"HP", "color" => "black", "type" => "Laptop", "price" => 300000}
-item2 = {"name" => "Macbook", "color" => "Silver", "type" => "Laptop", "price" => 500000}
-item3 = {"name" => "Dell", "color" => "red", "type" => "Laptop", "Price" => 400000}
+item1 = {name:" HP", "color" => "black", "type" => "Laptop", "price" => 300000}
+item2 = {name: "Macbook", "color" => "Silver", "type" => "Laptop", "price" => 500000}
+item3 = {name: "Dell", "color" => "red", "type" => "Laptop", :Price => 400000}
 
 puts item1[name]
+
+#{:a => 123}
