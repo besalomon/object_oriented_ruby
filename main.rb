2a8df1fd794c2d5dac037942dba406c596c7f3f0
Salomon Beza <beza.salomon@gmail.com>
2017-01-13 12:30:43 +0200

    Created two separate classes from the store_class

---
+++ main.rb
@@ -0,0 +1,4 @@
+require "./store_class.rb"
+product1 = Store.new({description: "HP", color: "black", type: "Laptop", price: 300000})
+product1.price = 500000
+product1.info
--- store_class.rb
+++ store_class.rb
@@ -1,9 +1,3 @@
-#item1 = {"name" => "HP", "color" => "black", "type" => "Laptop", "price" => 300000}
-#item2 = {"name" => "Macbook", "color" => "Silver", "type" => "Laptop", "price" => 500000}
-#item3 = {"name" => "Dell", "color" => "red", "type" => "Laptop", :Price => 400000}
-
-#puts item1[price
-#{:a => 123}
 
 class Store
 
@@ -22,16 +16,3 @@ def info
 end
 
 end
-
-product1 = Store.new({description: "HP", color: "black", type: "Laptop", price: 300000})
-product1.price = 500000
-product1.info
-# product2 = Store.new("Macbook", "Silver", "Laptop", 500000)
-# #puts product1.price
-# product3 = Store.new("Dell", "Red", "Laptop", 400000)
-
-# puts "#{product1.description} computer with type: #{product1.type} costs: #{product1.price}"
-# puts product2.price
-# puts product3.price
-
-#product2 = Product.new ("Mackbook", )
